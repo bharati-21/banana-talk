cc7af289a43a07d7d69ffecd90da0b105ecd9778
Bharati Subramanian <bharatisharada@gmail.com>
2021-06-30 23:02:49 +0530

    feat: Added Translation

--- main.js
+++ main.js
@@ -4,11 +4,24 @@
 var userText = document.querySelector("textarea[name='user-text']");
 var bananaText = document.querySelector("#banana-text");
 var btnTranslate = document.querySelector(".btn-translate");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
 
 btnTranslate.addEventListener("click", btnTranslateClick);
-
 function btnTranslateClick(e) {
-    console.log(userText.value);
+  console.log(userText.value);
+  const text = userText.value;
+
+  fetch(getTranslationURL(text))
+    .then((response) => response.json())
+    .then((json) => {
+      bananaText.innerHTML = json.contents.translated;
+    })
+    .catch((err) => console.log("error occurred " + err));
+}
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
 
 
