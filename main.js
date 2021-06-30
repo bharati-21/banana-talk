6d4c71959bd15c769f1f83bc51fd59a39dfd24d3
Bharati Subramanian <bharatisharada@gmail.com>
2021-06-30 12:54:48 +0530

    style: finished styles

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // var userName = prompt("What is your name?");
 // alert(`Hi, ${userName}. Welcome to Banana Talk!`);
 
-var btn = document.querySelector(".btn");
-btn.addEventListener("click", btnClick);
+// var btn = document.querySelector(".btn");
+// btn.addEventListener("click", btnClick);
 
-function btnClick(e) {
-  //alert("You clicked me!");
-  console.log("You clicked me!");
-}
+// function btnClick(e) {
+//   //alert("You clicked me!");
+//     console.log("You clicked me!");
+// }
 
 
