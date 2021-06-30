d595d4d53f8978e094fa3319b8e56da554b5d3ff
Bharati Subramanian <bharatisharada@gmail.com>
2021-06-30 22:23:34 +0530

    feat: displayed text from textbox

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // var userName = prompt("What is your name?");
 // alert(`Hi, ${userName}. Welcome to Banana Talk!`);
 
-var userText = document.querySelector("#user-text");
+var userText = document.querySelector("textarea[name='user-text']");
 var bananaText = document.querySelector("#banana-text");
 var btnTranslate = document.querySelector(".btn-translate");
 
 btnTranslate.addEventListener("click", btnTranslateClick);
 
 function btnTranslateClick(e) {
-  console.log(userText.value);
+    console.log(userText.value);
 }
 
 
